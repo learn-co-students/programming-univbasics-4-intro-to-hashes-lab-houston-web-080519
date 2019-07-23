e0be538f5548a5b40e6729e253eda37751e9f943
Emily M <mle.jo.11@gmail.com>
2019-07-23 15:53:31 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+ hash = Hash.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash => {"key" => "value", "other key" => "other value"}
 end
 
 def pioneer
